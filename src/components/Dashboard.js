d6a229f8cd4ac05e6255db700789766959ac93d3
Joseph Micla <josephmicla@gmail.com>
2021-12-16 16:15:20 -0800

    implements assignment logic, connects to websocket

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,128 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import classnames from "classnames";
+import Loading from "./Loading";
+import Panel from "./Panel";
+
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay,
+} from "helpers/selectors";
+import { setInterview } from "helpers/reducers";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    getValue: getTotalInterviews,
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    getValue: getLeastPopularTimeSlot,
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    getValue: getMostPopularDay,
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    getValue: getInterviewsPerDay,
+  },
+];
 
 class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+  // initial state
+  state = {
+    loading: true,
+    focused: null,
+    days: [],
+    appointments: {},
+    interviewers: {},
+  };
 
-    return <main className={dashboardClasses} />;
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+
+    this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+    this.socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+
+      if (typeof data === "object" && data.type === "SET_INTERVIEW") {
+        this.setState((previousState) =>
+          setInterview(previousState, data.id, data.interview)
+        );
+      }
+    };
+
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data,
+      });
+    });
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
+  selectPanel(id) {
+    this.setState((previousState) => ({
+      focused: previousState.focused !== null ? null : id,
+    }));
+  }
+
+  componentWillUnmount() {
+    this.socket.close();
+  }
+
+  // render
+  render() {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused,
+    });
+
+    const panels = data
+      .filter((data) => {
+        const { id } = data;
+        return this.state.focused === null || this.state.focused === id;
+      })
+      .map((data) => {
+        const { id, label, getValue } = data;
+        return (
+          <Panel
+            key={id}
+            id={id}
+            label={label}
+            value={getValue(this.state)}
+            onSelect={() => this.selectPanel(id)}
+          />
+        );
+      });
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
